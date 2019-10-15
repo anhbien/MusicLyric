f69d8508dc5d7e32f56214f9114672ab46403bc7
US\BIA1MTP <anh.bien@us.bosch.com>
2019-10-15 16:02:22 -0500

    Updates

---
+++ precache-manifest.4fe14c55c5a1df8bfaa704b593fc8ca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f907891b0be90518dfbf07956fe558",
+    "url": "/MusicLyric/index.html"
+  },
+  {
+    "revision": "49e3f3e2bc327d8928d1",
+    "url": "/MusicLyric/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4d059669f744511fb43e",
+    "url": "/MusicLyric/static/js/2.585ab26e.chunk.js"
+  },
+  {
+    "revision": "49e3f3e2bc327d8928d1",
+    "url": "/MusicLyric/static/js/main.a5f5ee02.chunk.js"
+  },
+  {
+    "revision": "c321ee7c0683aaa1cab2",
+    "url": "/MusicLyric/static/js/runtime-main.4a1de366.js"
+  }
+]);
