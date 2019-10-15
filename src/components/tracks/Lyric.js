dc6519f0484cc3f9b73df3dd572a3c9db5cdabc9
US\BIA1MTP <anh.bien@us.bosch.com>
2019-10-15 14:22:11 -0500

    + Display top 10 tracks from API

---
+++ src/components/tracks/Lyric.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Lyric extends Component {
+    render() {
+        return (
+            <div>
+                Lyric
+            </div>
+        );
+    }
+}
+
+export default Lyric;
