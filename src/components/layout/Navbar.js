ee26e6da9a21cb6453f74f176088aac1616154d9
US\BIA1MTP <anh.bien@us.bosch.com>
2019-10-15 15:53:49 -0500

    + Add Search feature

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-dark mb-5">
-                <span className="navbar-brand mb-0 h1 mx-auto">Music Lyric</span>
+                <span className="navbar-brand mb-0 h1 mx-auto">Music Lyrics</span>
             </nav>
         );
     }
